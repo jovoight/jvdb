ff6ccc8ced00e09e19f0e89765ac2db2e1e13344
Josiah Voight <jv@Josiahs-MBP.lan>
2024-09-16 17:44:21 -0500

    using stat function

--- filio.c
+++ filio.c
@@ -4,9 +4,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
+
+struct database_header_t {
+	unsigned short version;
+	unsigned short employees;
+	unsigned int filesize;
+};
 
 int main(int argc, char *argv[]) {
+	struct database_header_t head = {0};
+	struct stat db_stat = {0};
+
 	if (argc != 2) {
 		printf("Usage: %s <filename>\n", argv[0]);
 		return 0;
@@ -18,8 +26,24 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	char *mydata = "hello filio!\n";
-	write(fd, mydata, strlen(mydata));
+	if (read(fd, &head, sizeof(head)) != sizeof(head)) {
+		perror("read");
+		close(fd);
+		return -1;
+	}
+	
+	if (fstat(fd, &db_stat) != 0) {
+		perror("stat");
+		close(fd);
+		return -1;
+	}
+
+	printf("DB File Length, Reported by Stat: %llu\n", db_stat.st_size);
+	if (db_stat.st_size != head.filesize) {
+		printf("Hack attempted: Abort program.");
+		close(fd);
+		return -2;
+	}
 
 	close(fd);
 	return 0;
